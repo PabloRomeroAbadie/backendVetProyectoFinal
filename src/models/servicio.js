9ba5234f70a2d15dff7d62dfa26538f0c7f99117
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2022-02-24 18:14:28 -0300

    agregue servicio.controllers

--- src/models/servicio.js
+++ src/models/servicio.js
@@ -1,1 +1,19 @@
 //modelamos el dato a guardar en la base de datos
+import mongoose, {Schema} from "mongoose";
+
+const servicioSchema = new Schema({
+    servicioImg:{
+        data:Buffer,
+        Type:String
+    },
+    servicioTitle:{
+        type:String,
+    },
+    servicioText:{
+        type:String
+    }
+})
+
+const Servicio = mongoose.model("servicio", servicioSchema);
+
+export default Servicio;
