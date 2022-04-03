ef168ba6e278cb8d75e8a7b3134565db226f8ab2
Ramiro Nahuel Perez <ramironahuel101@gmail.com>
2022-04-02 21:18:20 -0300

    .

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/vetrolling";
+//const url = "mongodb://localhost:27017/rollingvet";
+const url = "mongodb+srv://vetrolling:154064742aS@cluster0.wki4i.mongodb.net/vetrolling";
 
 mongoose.connect(url)
 
